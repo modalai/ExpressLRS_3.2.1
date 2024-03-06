d65bef4fb1558ba583e5e318955e60e1bc25ddf8
modaljc <jacob.camarillO@modalai.com>
2024-03-06 14:42:41 -0800

    cleanup build script

--- src/build.sh
+++ src/build.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-num_variables=$#
-# echo $num_variables
-if [ "$num_variables" -gt 0 ]; then 
+
+VERSION=0
+if [ $# -gt 0 ]; then 
     ENCRYPT=$1
 else 
     ENCRYPT=0
 fi
-VERSION=0
 
 BUILD_DIR=".pio/build/MODALAI_M0139_via_UART"
 FW_VER="ModalAI_M0139-3.2.1.$VERSION.bin"
+TARGET="MODALAI_M0139_via_UART"
 
 # BUILD_DIR=".pio/build/MODALAI_Frsky_RX_R9MM_R9MINI_via_UART"
 # FW_VER="FrSky_R9Mini-3.2.1.$VERSION.bin"
+# TARGET="MODALAI_Frsky_RX_R9MM_R9MINI_via_UART"
 
 # Build application
-# platformio run --environment MODALAI_Frsky_RX_R9MM_R9MINI_via_UART # -v > build_output.txt
-platformio run --environment MODALAI_M0139_via_UART # -v > build_output.txt
-
+platformio run --environment $TARGET # -v > build_output.txt
 
 md5sum $BUILD_DIR/firmware.bin
 
@@ -26,15 +25,16 @@ if [ "$ENCRYPT" -eq 1 ]; then
     echo "Encrpyting application"
     stm32-encrypt $BUILD_DIR/firmware.bin $BUILD_DIR/$FW_VER 0E9B89FC1D97C8B7DC4B2EA4DAB2B3FF02153BCF1ED07E8617E3EFF59BB26806
 else
-    echo "Leaving application un-encypted"
+    echo "Leaving application un-encrypted"
     cp $BUILD_DIR/firmware.bin $BUILD_DIR/$FW_VER
 fi
 
 # Print out md5sum so we can verify what we push onto target matches what we just built and ecnrypted here
 md5sum $BUILD_DIR/$FW_VER
 
-adb push $BUILD_DIR/$FW_VER /usr/share/modalai/voxl-elrs/esptool/rx/
+# Copy/Push ELRS FW bin to desired location
+# adb push $BUILD_DIR/$FW_VER /usr/share/modalai/voxl-elrs/esptool/rx/
 
-cp $BUILD_DIR/$FW_VER .
-cp $BUILD_DIR/$FW_VER ../bins/
-cp $BUILD_DIR/$FW_VER /home/jacob/repos/voxl/voxl-elrs/test_bins
+# cp $BUILD_DIR/$FW_VER .
+# cp $BUILD_DIR/$FW_VER ../bins/
+# cp $BUILD_DIR/$FW_VER /home/jacob/repos/voxl/voxl-elrs/test_bins
--- src/clean.sh
+++ src/clean.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-VERSION=3
 # ENV="MODALAI_Frsky_RX_R9MM_R9MINI_via_UART"
 ENV="MODALAI_M0139_via_UART"
-BUILD_DIR=".pio/build/$ENV"
-FW_VER="FrSky_R9Mini-3.2.1.$VERSION.bin"
 
 # Clean application
 platformio run --target clean --environment $ENV
